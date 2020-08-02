938febc0308e3a24a0a05178ffdeaf32daa41d34
ThewBear <26767607+ThewBear@users.noreply.github.com>
2020-08-02 21:44:32 +0700

    Don't skip test-runners

--- _data/sites/test-runners.js
+++ _data/sites/test-runners.js
@@ -2,7 +2,7 @@ module.exports = {
 	description: "Front-end Testing Tools",
 	// skip if localhost
 	// skip if this is a new fork of the speedlify (not Zach’s)
-	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+	// skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
 	options: {
 		frequency: 60 * 11 + 30, // 11h, 30m
 		// Use "run" if the sites don’t share assets on the same origin
